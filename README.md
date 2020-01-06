b90f1b57ef38cd9186189a8cad3e0febd12f8713
Oleg Goncharov <oleg.gnch@gmail.com>
2020-01-06 22:25:02 +0300

    modified solution, added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Euler. Problem 96
+
+Solving 50 Su Doku puzzles from [Project Euler](https://projecteuler.net/problem=96)
--- solution.py
+++ solution.py
@@ -1,10 +1,20 @@
 from sudoku_solver import Sudoku
 
+solved = 0
+result = 0
+
 with open('p096_sudoku.txt', 'r') as f:
     line = f.readline()
     while line.startswith('Grid'):
-        print(line)
+        print(line.strip())
         matrix = [[int(ch) for ch in f.readline().strip()] for i in range(9)]
         sudoku = Sudoku(matrix)
+        sudoku.solve()
+        if sudoku.unsolved_cells == 0:
+            solved += 1
+            result += (sudoku.cells[0][0].value * 100 + sudoku.cells[0][1].value * 10 + sudoku.cells[0][2].value)
         print(sudoku)
         line = f.readline()
+
+print(f'Решено {solved} судоку из 50')
+print(f'Ответ: {result}')
